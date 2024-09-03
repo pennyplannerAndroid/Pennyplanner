8557fce4e2738b1340ff634f54719400ad6c73ce
AseemKumar2014 <AseemKumar2014@gmail.com>
2024-09-04 02:49:22 +0530

    refactoring and cleanups

--- app/src/main/java/com/penny/planner/ui/activities/OnboardingActivity.kt
+++ app/src/main/java/com/penny/planner/ui/activities/OnboardingActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel

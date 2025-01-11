82258f4acc9c5030aeb86b773f9627777b9c1fa0
AseemKumar2014 <AseemKumar2014@gmail.com>
2025-01-11 17:52:25 +0530

    monthlybudget variable change from double to string for server

--- app/src/main/java/com/penny/planner/models/MonthlyBudgetInfoModel.kt
+++ app/src/main/java/com/penny/planner/models/MonthlyBudgetInfoModel.kt
@@ -3,4 +3,10 @@ package com.penny.planner.models
 data class MonthlyBudgetInfoModel (
     var monthlyBudget: Double = 0.0,
     var safeToSpendLimit: Int = 80
-)
+) {
+    fun toFireBaseEntity() =
+        mapOf(
+            Pair("monthlyBudget", monthlyBudget.toString()),
+            Pair("safeToSpendLimit", safeToSpendLimit)
+        )
+}
